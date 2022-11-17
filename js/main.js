bbacd97ac4e5dc7bec1d28ed44c4de61dc9b632f
PressJump <40802367+PressJump@users.noreply.github.com>
2022-11-18 10:14:50 +1100

    💄 Firefox Theme Colors Fixes #1

--- js/main.js
+++ js/main.js
@@ -86,3 +86,59 @@ async function saveToStorage({title: title, iconUrl: iconUrl, webviewUrl: webvie
 }
 
 document.getElementById("addPageButton").addEventListener("click", addPage);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function setSidebarStyle(theme) {
+  const body = document.body;
+
+  if (theme.colors && theme.colors.frame) {
+    body.style.backgroundColor =
+      theme.colors.frame;
+  } else {
+    body.style.backgroundColor = "white";
+  }
+
+  if (theme.colors && theme.colors.toolbar) {
+    body.style.backgroundColor = theme.colors.toolbar;
+  } else {
+    body.style.backgroundColor = "#ebebeb";
+  }
+  
+  if (theme.colors && theme.colors.toolbar_text) {
+    body.style.color = theme.colors.toolbar_text;
+  } else {
+    body.style.color = "black";
+  }
+}
+
+// Set the element style when the extension page loads
+async function setInitialStyle() {
+  const theme = await browser.theme.getCurrent();
+  setSidebarStyle(theme);
+}
+setInitialStyle();
+
+// Watch for theme updates
+browser.theme.onUpdated.addListener(async ({ theme, windowId }) => {
+  const sidebarWindow = await browser.windows.getCurrent();
+  /*
+    Only update theme if it applies to the window the sidebar is in.
+    If a windowId is passed during an update, it means that the theme is applied to that specific window.
+    Otherwise, the theme is applied globally to all windows.
+  */
+  if (!windowId || windowId == sidebarWindow.id) {
+    setSidebarStyle(theme);
+  }
+});
